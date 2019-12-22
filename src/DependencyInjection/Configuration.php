efd8282135b70135f50548d76377c3eb02ca2adf
Alexandre T <alexandre.Tranchant@gmail.com>
2019-12-22 12:42:59 +0100

    Update Configuration.php

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,11 +2,11 @@
 
 namespace Ekyna\Bundle\PayumMoneticoBundle\DependencyInjection;
 
-use App\Kernel;
 use Ekyna\Component\Payum\Monetico\Api\Api;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\HttpKernel\Kernel;
 
 /**
  * Class Configuration
